ea0c603f8eba4554a4383344dc09a0f6182756f9
azimbek <voron75voron@gmail.com>
2026-03-06 07:35:02 +0500

    auth is done, start ORM

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 #  Our function recievs  request
 # def home(request):
@@ -17,6 +18,10 @@ def home(request):
 def error(req):
     return HttpResponse('error!')
 
+
+@login_required(login_url='admin/')
+def authorized(request):
+    return render(request,'home/authorized.html', {})
 
 
 # Create your views here.
