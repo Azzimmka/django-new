64e8b39dfef8fa4487faa72c03d5a2492a98a826
azimbek <voron75voron@gmail.com>
2026-03-06 08:04:03 +0500

    created our first model Noetes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Notes(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(default=timezone.now, null=True, blank=True)
