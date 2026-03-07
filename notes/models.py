f81e66589d5431872f0273700ecb2005b2b542dd
azimbek <voron75voron@gmail.com>
2026-03-07 17:05:04 +0500

    add new field likes

--- notes/models.py
+++ notes/models.py
@@ -5,3 +5,4 @@ class Notes(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     text = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
